7a50f4e724bb92110481d2b6f0acb104edbfccb0
Clément <clementkeller.ck@gmail.com>
2021-06-21 14:54:26 +0200

    Adding heartLivesCounter

---
+++ Assets/Scripts/Other/LivesCounter.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LivesCounter : MonoBehaviour
+{
+    public static int remainingLives;
+    private Text livesText;
+
+    void Start()
+    {
+        livesText = GetComponent<Text>();
+        remainingLives = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        livesText.text = remainingLives.ToString();
+    }
+}
