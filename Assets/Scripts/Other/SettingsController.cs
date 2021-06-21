d9e63ae8be25dd3fffe6260a8b00c1aeea52f824
Victor <victorbultez2@gmail.com>
2021-06-21 15:56:49 +0200

    Adding volume slider on settings & linking it to script & audio file

--- Assets/Scripts/Other/SettingsController.cs
+++ Assets/Scripts/Other/SettingsController.cs
@@ -2,14 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class SettingsController : MonoBehaviour
 {
+    public AudioMixer audioMixer;
 
     public void backButton()
     {
         SceneManager.LoadScene("Menu");
     }
 
-
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume",volume);
+    }
 }
