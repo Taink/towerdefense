726e80618f86a5a125feac6ed21be63c6e4e657d
Manil <manil.richard@gmail.com>
2021-06-22 16:15:54 +0200

    Big round update

--- Assets/Scripts/Other/RoundController.cs
+++ Assets/Scripts/Other/RoundController.cs
@@ -10,11 +10,18 @@ public class RoundController : MonoBehaviour
     public GameObject mageEnemy;
     public GameObject tankEnemy;
 
+    private const float basicTime = (float)0.75;
+    private const float dogTime = (float)0.50;
+    private const float mageTime = (float)1.25;
+    private const float tankTime = (float)1.25;
+
     public float timeBetweenWaves;
     public float timeBeforeRoundStart;
     private float timeVariable;
     public int round;
     private int lastRound;
+    private int power = 2;
+    private int compteur;
     public Text roundText;
 
     private bool isRoundGoing;
@@ -29,24 +36,69 @@ public class RoundController : MonoBehaviour
 
         timeVariable = Time.time + timeBeforeRoundStart;
         round = 1;
-        lastRound = 20;
+        lastRound = 10;
         updateRound();
     }
 
 
+
     private void spawnEnemies()
     {
-        StartCoroutine("ISpawnEnemies");
+        compteur = 0;
+        power = power + 3;
+        if(round < 5)
+        {
+            StartCoroutine(ISpawnEnemies(5, basicEnemy, basicTime));
+            compteur += 5;
+        }
+        if (round >= 3 & round < 5)
+        {
+            StartCoroutine(ISpawnEnemies((round-2)*2, dogEnemy, dogTime));
+            compteur++;
+        }
+        if (round >=5 )
+        {
+            power++;
+            while (compteur < power)
+            {
+                int rint = Random.Range(0, 4);
+                switch (rint)
+                {
+                    case 0:
+                        StartCoroutine(ISpawnEnemies(2, basicEnemy, basicTime));
+                        compteur += 2;
+                        break;
+                    case 1:
+                        StartCoroutine(ISpawnEnemies(2, dogEnemy, dogTime));
+                        compteur += 2;
+                        break;
+                    case 2:
+                        StartCoroutine(ISpawnEnemies(2, mageEnemy, mageTime));
+                        compteur += 4;
+                        break;
+                    case 3:
+                        StartCoroutine(ISpawnEnemies(2, tankEnemy, tankTime));
+                        compteur += 4;
+                        break;
+
+
+                }
+            }
+        }
+
+        StartCoroutine(ISpawnEnemies(power - compteur, basicEnemy, (float)0.5));
+
     }
 
-    IEnumerator ISpawnEnemies()
+    IEnumerator ISpawnEnemies(int nb, GameObject enemy, float time) //nombre d'ennemis, type, temps entre chaque spawn
     {
-        for(int i = 0; i < round; i++)
+        for(int i = 0; i < nb; i++)
         {
-            GameObject newEnemy = Instantiate(basicEnemy, MapGenerator.getStartCoords(), Quaternion.identity);
-            yield return new WaitForSeconds(1f);
+            GameObject newEnemy = Instantiate(enemy, MapGenerator.getStartCoords(), Quaternion.identity);
+            yield return new WaitForSeconds(time);
         }
     }
+
 
     private void Update()
     {
